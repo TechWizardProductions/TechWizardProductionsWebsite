3a324502999f98c1c07aa1dfab4ce9662fe3783c
TechWizardProductions <31485683+TechWizardProductions@users.noreply.github.com>
2017-09-04 11:24:51 +0200

    Cleaned up cide after PHP 7 port

--- account/index.php
+++ account/index.php
@@ -1,10 +1,11 @@
+<?php
+    $rootdir = "../";
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<!-- Last edited on 16/02/2017 -->
+<!-- Last edited on 04/09/2017 -->
 <head>
-    <?php
-        $rootdir = "../";
-    ?>
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="<?php echo $rootdir ?>style/style.css">
     <link rel="icon" type="image/x-icon" href="<?php echo $rootdir; ?>images/logoSmall.ico">
@@ -13,7 +14,6 @@
 <body>
     <?php
         include($rootdir . "style/header.inc.php");
-
         include($rootdir . "style/navigation.inc.php");
     ?>
     <div id="content">
@@ -24,12 +24,12 @@
            echo "<h1> Account Information </h1>";
            echo "On this page your account information will be displayed and you can update this information. <br />";
            echo '<a href="edit.php">Edit<img src="' . $rootdir . 'images/edit.png" width="20" heigth="200"></a>';
-            include($rootdir . "account/info.php");
-            include($rootdir . "account/logOut.inc.php");
+           include($rootdir . "account/info.php");
+           include($rootdir . "account/logOut.inc.php");
         } else {
         echo 'This session has timed out. Please log in again <a href="'. $rootdir .'/register-login">here</a>';
         }
-            include($rootdir . "style/footer.inc.php");
+           include($rootdir . "style/footer.inc.php");
         ?>
     </div>
 </body>
